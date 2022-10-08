65c7b148e98484f95325993e5177e0d6fb644a4d
Saturnyasha <t.akhlamionak@gmail.com>
2022-10-08 21:16:13 +0300

    fix

--- Prototype and Inheritance.js
+++ Prototype and Inheritance.js
@@ -1,31 +1,13 @@
 /* Create 2 objects: animal and cat, add move property to animal object, cat object must inherit move property from object animal
  Создайте 2 объекта: животное и кошка, добавьте свойство движения к объекту животного, /объект кошки должен наследовать свойство движения от объекта животного.*/
 
-let Animal1 = {
-    move() {
-        console.log(`${this.name} запрыгнул в коробочку и замурчал`)
+class Animal {
+    constructor(name,age,color){
+        this.name = name;
+        this.age = age;
+        this.color = color;
     }
-};
-
-class Cat1 {
-    constructor(name) {
-         this.name = name;
-    }
-};
-
-Object.setPrototypeOf(Cat1.prototype, Animal1);
-let kitty = new Cat1("Апельсин");
-kitty.move();
-
-//OR//
-
-class Animal { //create obj Animal
-    constructor(options){
-        this.name = options.name;
-        this.age = options.age;
-        this.color = options.color;
-    }
-move(){ //add method
+move(){
      console.log(`${this.name} запрыгнул в коробочку`)
     }
 };
